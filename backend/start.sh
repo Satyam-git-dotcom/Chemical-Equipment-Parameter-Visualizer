97223a7482fab84aebc48fd19d8563a1a2aac011
Satyam Vishwakarma <satyamvishwakarma@Satyams-MacBook-Air-3.local>
2026-02-10 00:55:25 +0530

    Fix Render start.sh path to manage.py

--- backend/start.sh
+++ backend/start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+cd backend
+
 python manage.py migrate
 python manage.py collectstatic --noinput
 
 gunicorn equipment_visualizer.wsgi:application \
   --bind 0.0.0.0:$PORT \
-  --workers 2s
+  --workers 1
