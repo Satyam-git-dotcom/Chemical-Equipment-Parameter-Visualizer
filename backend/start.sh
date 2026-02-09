e6edea7cf5e6d2e29a894e5eb95af21ed42a2848
Satyam Vishwakarma <satyamvishwakarma@Satyams-MacBook-Air-3.local>
2026-02-10 00:46:02 +0530

    Updating

--- backend/start.sh
+++ backend/start.sh
@@ -4,4 +4,6 @@ set -o errexit
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-gunicorn equipment_visualizer.wsgi:application
+gunicorn equipment_visualizer.wsgi:application \
+  --bind 0.0.0.0:$PORT \
+  --workers 2s
